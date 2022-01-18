0ad78b1ee07bccd162b06621d33b8deb2e59ceed
SeoLeX <71733556+SeoLeX@users.noreply.github.com>
2022-01-18 13:36:20 +0300

    commit

--- basic-password-generator.py
+++ basic-password-generator.py
@@ -1,1 +1,18 @@
+import random
+import string
 
+print("Merhaba! SeoLeX'in şifre üreticine hoşgeldin.")
+
+length = int(input("\nOluşturmak istediğin şifrenin karakter sayısını gir! \n"))
+lower = string.ascii_lowercase
+upper = string.ascii_uppercase
+number = string.digits
+symbol = string.punctuation
+
+all = lower + upper + number + symbol
+
+temp = random.sample(all,length)
+
+password ="".join(temp)
+
+print(password)
